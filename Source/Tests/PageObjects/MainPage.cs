0408deafbbd15ed77a5adfa66dc84a2b2894b8b4
nastysilence <maxse828@gmail.com>
2021-07-17 13:47:11 +0300

    add startup and begin work on pageObject

---
+++ Source/Tests/PageObjects/MainPage.cs
@@ -0,0 +1,17 @@
+﻿using Core.Base.Elements;
+using OpenQA.Selenium;
+
+namespace Tests.PageObjects
+{
+    public class MainPage
+    {
+        private IWebDriver _driver { get; }
+
+        public MainPage(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        private IWebElement ProductSection(string productName) => ElementSearch.FindElement(_driver, "");
+    }
+}
